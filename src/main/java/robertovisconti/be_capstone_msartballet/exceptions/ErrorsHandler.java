9dd7a8c6c0e92cde3b99cbea2a6b6eee7dda57d3
Roberto Visconti <robertovisconti93@gmail.com>
2026-08-04 08:14:10 +0200

    Add error handler
    Aggiunto gestione parametro e gestione richiesta non leggiggibile con errore 400

--- src/main/java/robertovisconti/be_capstone_msartballet/exceptions/ErrorsHandler.java
+++ src/main/java/robertovisconti/be_capstone_msartballet/exceptions/ErrorsHandler.java
@@ -2,11 +2,13 @@ package robertovisconti.be_capstone_msartballet.exceptions;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import robertovisconti.be_capstone_msartballet.payloadsDTO.errorsDTO.ErrorsDTO;
 
 
@@ -71,7 +73,22 @@ public class ErrorsHandler {
         return new ErrorsDTO("Non hai i permessi per eseguire questa operazione", LocalDateTime.now());
     }
 
-    // 6. GESTIONE DI QUALSIASI ALTRA ECCEZIONE IMPREVISTA (500 Internal Server Error)
+    // 6. GESTIONE PARAMETRO O PATH VARIABLE CON TIPO ERRATO (400 Bad Request)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorsDTO handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String messaggio = "Il valore '" + ex.getValue() + "' non è valido per il parametro '" + ex.getName() + "'";
+        return new ErrorsDTO(messaggio, LocalDateTime.now());
+    }
+
+    // 7. GESTIONE CORPO DELLA RICHIESTA NON LEGGIBILE / JSON MALFORMATO (400 Bad Request)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorsDTO handleMalformedJson(HttpMessageNotReadableException ex) {
+        return new ErrorsDTO("Il corpo della richiesta non è un JSON valido", LocalDateTime.now());
+    }
+
+    // 8. GESTIONE DI QUALSIASI ALTRA ECCEZIONE IMPREVISTA (500 Internal Server Error)
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorsDTO handleGenericError(Exception ex) {
